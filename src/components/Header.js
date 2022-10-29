6514a0d24d550028ecd5a08c7b0deb6fc272c140
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-29 15:02:06 +0100

    Create the Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+export default function Header(){
+    return(
+        <header>
+            <h1>
+                <HighlightIcon />
+                ToDoList
+            </h1>
+        </header>
+    )
+}
